fb1a333a9eece99dc0c1f48747a29aececf6225a
Will Roden <will@roden.cc>
2023-08-10 18:15:07 -0500

    updates

--- test-setup-go-faster_test.go
+++ test-setup-go-faster_test.go
@@ -1,11 +1,14 @@
 package testsetupgofaster
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestNothing(t *testing.T) {
+	fmt.Println("starting test")
 	require.True(t, true)
+	fmt.Println("ending test")
 }
